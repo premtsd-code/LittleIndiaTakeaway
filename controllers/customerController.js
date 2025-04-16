dcfdb5e3792ef6cc998e9b81756d973d2792d2f5
PREM PALANISAMY <prem.palanisamy.2025@mumail.ie>
2025-04-16 18:39:29 +0100

    bugfix

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,4 +1,4 @@
-const Order = require('../models/Order');
+const Order = require('../models/OrderSchema');
 const DeliverySlot = require('../models/DeliverySlot');
 const FoodItem = require('../models/FoodItem');
 
