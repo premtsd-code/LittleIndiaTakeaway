cb17593de8e17a9dfba28b03d8d73e134c6fe6d3
PREM PALANISAMY <prem.palanisamy.2025@mumail.ie>
2025-04-17 11:56:33 +0100

    createOrder removed

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-// Route to create a new order
-router.post('/', orderController.createOrder);
 
 // Route to get all orders
 router.get('/', orderController.getAllOrders);
